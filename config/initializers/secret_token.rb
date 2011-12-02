862e608e6b4c96b46cd32f01f6d06d4b93f688a8
shonoru <shonoru@gmail.com>
2011-12-03 03:38:52 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurrencyScheduler::Application.config.secret_token = '52c0d57788d0bc6c281c27f1d2b72d62afd86df0672556c44b0a588593e819923353848a05f895e8d05455c2fc45e44d058ff43844b97b6d3f8ebc90bd48aeeb'
